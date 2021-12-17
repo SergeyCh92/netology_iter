4215ea8c56c43b5c5dfdd6edba0fa343f911bdc7
Sergey Chaban <loriston@rambler.ru>
2021-12-17 17:11:55 +0300

    Задание по декораторам

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import datetime
+
+
+path = 'C:/Users/Сергей/Desktop/'
 nested_list = [
     ['a', 'b', 'c'],
     ['d', 'e', 'f', 'h', False],
     [1, 2, None]]
 
 
+def decorator_function(my_path):
+    def _decorator_function(func):
+        def wrapper(*args, **kwargs):
+            current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            result = func(*args, **kwargs)
+            with open(f'{my_path}log.txt', 'a') as f:
+                f.write(f'{current_datetime}, название функции\\метода - {func.__name__}, аргументы функции - {args, kwargs},'
+                        f'возвращаемое значение - {result}.\n')
+        return wrapper
+    return _decorator_function
+
+
+decor = decorator_function(path)
+
+
 class MyIter:
 
+    @decor
     def __init__(self, nest_list: list):
         self.nest_list = nest_list
         self.cursor = 0
@@ -14,6 +34,7 @@ class MyIter:
     def __iter__(self):
         return self
 
+    @decor
     def __next__(self):
         if self.cursor_nested + 1 < len(self.nest_list[self.cursor]):
             self.cursor_nested += 1
